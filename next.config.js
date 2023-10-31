d08dc3c3d690f733596157e5a57e96d83c625695
Jimmyyy <chenjimmy91@hotmail.com>
2023-10-31 15:02:16 +0800

    home page and signin page updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // headlessui React 18 compatibility tracking issue
+    // add this line of code when `npm run dev`
+    // https://github.com/tailwindlabs/headlessui/issues/681
+    reactStrictMode: false,
+
+}
 
 module.exports = nextConfig
