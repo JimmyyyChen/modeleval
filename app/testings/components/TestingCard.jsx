a048ef207c420804c034abab1f53df1bfe58f7fe
Jimmyyy <chenjimmy91@hotmail.com>
2023-11-16 10:12:11 +0800

    Add progress validation and task details to TestingCard component

--- app/testings/components/TestingCard.jsx
+++ app/testings/components/TestingCard.jsx
@@ -1,23 +1,61 @@
 import React from "react";
 
-export default function TestingCard() {
+export default function TestingCard({
+  name,
+  size, // in GB
+  startTime,
+  endTime,
+  progress,
+  type,
+}) {
+  if (progress < 0 || progress > 100) {
+    throw new Error("Progress should be between 0 to 100");
+  }
+
+  if (progress !== 100 && endTime !== null) {
+    throw new Error("End time should be null if progress is not 100");
+  }
+
+  if (progress === 100 && typeof endTime === null) {
+    throw new Error("End time should not be null if progress is 100");
+  }
+
+  const completed = progress === 100;
+
+  // convert time to string. e.g. 15:20:03
+  const formatedStartTime = new Date(startTime).toLocaleTimeString("en-US", {
+    hour12: false,
+  });
+
+  const formatedEndTime = new Date(endTime).toLocaleTimeString("en-US", {
+    hour12: false,
+  });
+
   return (
-    <div className="w-full flex-wrap items-center overflow-hidden rounded-3xl bg-base-100 p-3 shadow-xl sm:flex">
+    <div className="w-full flex-wrap items-center overflow-hidden rounded-3xl bg-base-100 p-2 shadow-xl sm:flex m-3">
       <div className="p-3">
-        <h2 className="text-xl font-bold">Example Task</h2>
-        <p className="text-gray-500">已测试14GB • 14:00 开始</p>
+        <h2 className="text-xl font-bold">{name}</h2>
+        {completed ? (
+          <p className="text-gray-500">
+            共{size}GB • {formatedStartTime} 开始 • {formatedEndTime} 结束
+          </p>
+        ) : (
+          <p className="text-gray-500">
+            共{size}GB • {formatedStartTime} 开始
+          </p>
+        )}
       </div>
 
       <div className="ml-auto space-x-3 p-3">
         <div
           className="radial-progress"
-          style={{ "--value": 70, "--size": "3rem" }}
+          style={{ "--value": progress, "--size": "3.3rem" }}
           role="progressbar"
         >
-          70%
+          {progress}%
         </div>
 
-        <button className="btn btn-neutral rounded-full ">客观评测</button>
+        <button className="btn btn-neutral rounded-full ">{type}</button>
       </div>
     </div>
   );
--- app/testings/page.js
+++ app/testings/page.js
@@ -1,9 +1,32 @@
 import TestingCard from "./components/TestingCard";
 
+// examples
+const tasks = [
+  {
+    name: "Example Task",
+    size: 1.24,
+    startTime: new Date().getTime(),
+    // endTime is null means the task is still running
+    endTime: null,
+    progress: 72,
+    type: "客观评测",
+  },
+  {
+    name: "Example Task2",
+    size: 2,
+    startTime: new Date().getTime(),
+    endTime: new Date().getTime(),
+    progress: 100,
+    type: "客观评测",
+  },
+];
+
 export default function TestingsPage() {
   return (
     <div className="w-full">
-      <TestingCard />
+      {tasks.map((task) => (
+        <TestingCard {...task} />
+      ))}
     </div>
   );
 }
