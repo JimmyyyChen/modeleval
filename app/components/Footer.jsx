488549c5bea3224f1ca9259a37136d8e6ab61b17
zhangzhiyuan <zhangzhiyuan@swellai.net>
2023-11-19 15:52:10 +0800

    show single dataset

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="footer mx-auto flex flex-col items-center space-y-6 p-4 px-2 sm:flex-row sm:justify-between sm:space-y-0 sm:px-6 lg:px-8">
+    <footer className="footer mx-auto flex flex-col items-center space-y-6 py-2 px-2 sm:flex-row sm:justify-between sm:space-y-0 sm:px-6 lg:px-8">
       <div className="flex flex-shrink-0 flex-col items-center space-y-4 sm:h-full sm:flex-row sm:space-x-6 sm:space-y-0">
         <div className="flex flex-shrink-0 items-center">
           <Link href="/">
