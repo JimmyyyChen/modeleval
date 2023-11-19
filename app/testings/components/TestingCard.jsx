db5cf3ccd5c08c25cb753c3140ad160c8b0c7aac
Jimmyyy <chenjimmy91@hotmail.com>
2023-11-19 22:49:19 +0800

    Refactor TestingCard component and update TestingsPage to fetch testings from the database

--- app/testings/components/TestingCard.jsx
+++ app/testings/components/TestingCard.jsx
@@ -2,46 +2,43 @@ import React from "react";
 
 export default function TestingCard({
   name,
-  size, // in GB
+  sizeInMB,
   startTime,
   endTime,
-  progress,
+  taskCount,
+  completedTaskCount,
   type,
 }) {
-  if (progress < 0 || progress > 100) {
-    throw new Error("Progress should be between 0 to 100");
-  }
 
-  if (progress !== 100 && endTime !== null) {
-    throw new Error("End time should be null if progress is not 100");
-  }
+  const progress = Math.round((completedTaskCount / taskCount) * 100);
+  const completed = completedTaskCount === taskCount;
 
-  if (progress === 100 && typeof endTime === null) {
-    throw new Error("End time should not be null if progress is 100");
-  }
+  const sizeInGB = (sizeInMB / 1024).toFixed(2); 
 
-  const completed = progress === 100;
-
-  // convert time to string. e.g. 15:20:03
-  const formatedStartTime = new Date(startTime).toLocaleTimeString("en-US", {
+  const formatedStartTime = startTime.toLocaleTimeString("en-US", {
     hour12: false,
   });
 
-  const formatedEndTime = new Date(endTime).toLocaleTimeString("en-US", {
-    hour12: false,
-  });
+  let formatedEndTime = "";
+  if (endTime) {
+    formatedEndTime = endTime.toLocaleTimeString("en-US", {
+      hour12: false,
+    });
+  }
+
 
   return (
-    <div className="w-full flex-wrap items-center overflow-hidden rounded-3xl bg-base-100 p-2 shadow-xl sm:flex m-3">
+    <div className="m-3 w-full flex-wrap items-center overflow-hidden rounded-3xl bg-base-100 p-2 shadow-xl sm:flex">
       <div className="p-3">
         <h2 className="text-xl font-bold">{name}</h2>
         {completed ? (
           <p className="text-gray-500">
-            共{size}GB • {formatedStartTime} 开始 • {formatedEndTime} 结束
+            共{sizeInGB}GB • {formatedStartTime} 开始 • {formatedEndTime}{" "}
+            结束
           </p>
         ) : (
           <p className="text-gray-500">
-            共{size}GB • {formatedStartTime} 开始
+            共{sizeInGB}GB • {formatedStartTime} 开始
           </p>
         )}
       </div>
--- app/testings/page.js
+++ app/testings/page.js
@@ -1,31 +1,21 @@
 import TestingCard from "./components/TestingCard";
+import prisma from "@/lib/prisma";
 
-// examples
-const tasks = [
-  {
-    name: "Example Task",
-    size: 1.24,
-    startTime: new Date().getTime(),
-    // endTime is null means the task is still running
-    endTime: null,
-    progress: 72,
-    type: "客观评测",
-  },
-  {
-    name: "Example Task2",
-    size: 2,
-    startTime: new Date().getTime(),
-    endTime: new Date().getTime(),
-    progress: 100,
-    type: "客观评测",
-  },
-];
-
-export default function TestingsPage() {
+export default async function TestingsPage() {
+  const testings = await prisma.testing.findMany();
   return (
     <div className="w-full">
-      {tasks.map((task) => (
-        <TestingCard {...task} />
+      {/* print all testings */}
+      {testings.map((testing) => (
+        <TestingCard
+          name={testing.name}
+          type={testing.type}
+          sizeInMB={testing.sizeInMB}
+          taskCount={testing.taskCount}
+          completedTaskCount={testing.completedTaskCount}
+          startTime={testing.startTime}
+          endTime={testing.endTime}
+        />
       ))}
     </div>
   );
