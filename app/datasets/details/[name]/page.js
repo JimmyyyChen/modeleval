a0cda4e98b2d7fd75442ab58c1ba48bdecce0f9b
zhangzhiyuan <zhangzhiyuan@swellai.net>
2023-12-05 20:17:52 +0800

    Modularized the datasets_UI

--- app/datasets/details/[name]/page.js
+++ app/datasets/details/[name]/page.js
@@ -9,7 +9,7 @@ import {
 } from "./data";
 import Community from "@/app/components/Community";
 import MainInfoDisplay from "@/app/components/MainInfoDisplay";
-import DataDisplay from "../DataDisplay";
+import ItemsDisplay from "../ItemsDisplay";
 
 export default function Home({ params: { name } }) {
   return (
@@ -21,7 +21,7 @@ export default function Home({ params: { name } }) {
       </div>
 
       <div className="mt-6 flex h-full w-full flex-wrap items-start space-x-0 space-y-6 p-4 sm:flex-nowrap sm:space-x-6 sm:space-y-0 ">
-        <div className="flex h-full w-full flex-col space-y-4 sm:w-1/3">
+        <div className="flex h-full w-full flex-col space-y-4 p-4 sm:w-1/3">
           <button className="btn btn-primary w-full text-white">
             上传条目
           </button>
@@ -34,12 +34,12 @@ export default function Home({ params: { name } }) {
         </div>
 
         <div className="h-full w-full p-4 sm:w-2/3">
-          <DataDisplay items={datasetItems} pages={pages} />
+          <ItemsDisplay items={datasetItems} />
         </div>
       </div>
 
-      <div className="w-full p-4">
-        <Community issues={issues} fontsize="text-4xl" textcontent="留言" />
+      <div className="w-full p-4 text-4xl">
+        <Community issues={issues} textcontent="留言" />
       </div>
     </>
   );
