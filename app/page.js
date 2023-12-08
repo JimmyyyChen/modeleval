4e3bc0f70e9b9f32ad39f34c50eb57a87af0d674
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-09 00:22:21 +0800

    Add DataGrid component for displaying model evaluation results

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
+"use client";
 import { SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
+
+import { DataGrid } from "@mui/x-data-grid";
 
 export default function HomePage() {
   return (
@@ -11,9 +14,10 @@ export default function HomePage() {
         </h1>
       </div>
       <p className="mt-6 text-lg leading-8 text-gray-600">
-        Anim autee id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-        cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        ModelEval旨在为用户提供一个全面评估和比较不同大模型能力的平台。它
+        将提供一个开源免费的评测平台。开发者可以在这个平台上上传自己的数据集，并对不同模型在不同数据集上的性能进行包括客观评测、主观评测和对抗性评测在内的全面评估和比较。ModelEval将提供详细的模型能力评测榜单，帮助开发者了解各个模型的优势和限制。
       </p>
+      {/* 当登出时显示注册登录按钮 */}
       <SignedOut>
         <div className="justify-left mt-10 flex items-center gap-x-3">
           <Link href="/sign-up" className="btn btn-accent">
@@ -24,6 +28,33 @@ export default function HomePage() {
           </Link>
         </div>
       </SignedOut>
+
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+      />
     </div>
   );
 }
+
+const columns = [
+  { field:"id", headerName: "ID", width: 70},
+  { field: "dataset", headerName: "数据集", width: 130 },
+  { field: "GPT4_automated", headerName: "GPT4客观测试", width: 130 },
+  { field: "GPT3_automated", headerName: "GPT3.5客观测试", width: 130 },
+  { field: "GPT4_human", headerName: "GPT4主观测试", width: 130 },
+  { field: "GPT3_human", headerName: "GPT3.5主观测试", width: 130 },
+  { field: "GPT4_comparative", headerName: "GPT4对抗测试", width: 130 },
+  { field: "GPT3_comparative", headerName: "GPT3.5对抗测试", width: 130 },
+];
+
+const rows = [
+  { id: 1, dataset: "mmlu_select", GPT4_automated: "0.5", GPT3_automated: "0.6", GPT4_human: "untested", GPT3_human: "untested", GPT4_comparative: "untested", GPT3_comparative: "untested" },
+  { id: 2, dataset: "zbench_common", GPT4_automated: "untested", GPT3_automated: "untested", GPT4_human: "0.7", GPT3_human: "0.8", GPT4_comparative: "0.9", GPT3_comparative: "0.9"}
+]
