35287503814590274b995e6e45d3b3df19acfeea
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-16 21:02:55 +0800

    创建新测试页面更新

--- app/testings/new-testing/page.js
+++ app/testings/new-testing/page.js
@@ -4,28 +4,60 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@clerk/nextjs";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 import DatasetOption from "./components/DatasetOption";
 import ModelOption from "./components/ModelOption";
 
 export default function NewTestingPage() {
   const router = useRouter();
-  const { userId, isLoaded } = useAuth();
+  const { userId } = useAuth();
 
-  const addDemoTesting = async () => {
-    if (!isLoaded || !userId) {
-      return null;
+  const [datasets, setDatasets] = useState([]);
+  const [models, setModels] = useState([]);
+
+  const [selectedDataset, setSelectedDataset] = useState(null);
+  const [selectedTestingMethod, setSelectedTestingMethod] = useState(null);
+  const [selectedModels, setSelectedModels] = useState([]);
+
+  useEffect(() => {
+    const fetchDatasets = async () => {
+      try {
+        const res = await axios.get("/api/datasets");
+        setDatasets(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDatasets();
+
+    const fetchModels = async () => {
+      try {
+        const res = await axios.get("/api/models");
+        setModels(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchModels();
+  }, []);
+
+  const addTesting = async () => {
+    if (
+      !selectedDataset ||
+      !selectedTestingMethod ||
+      selectedModels.length === 0
+    ) {
+      return;
     }
     try {
-      await axios.post("/api/testings", {
+      await axios.post("/api/testings/operations/addTask", {
         userId: userId,
-        name: "mmlu_select 客观测试",
-        sizeInMB: 101,
-        startTime: new Date(),
-        endTime: null,
-        taskCount: 100,
-        completedTaskCount: 70,
-        type: "客观测试",
+        taskName: `${selectedDataset.datasetName} ${selectedTestingMethod}`,
+        startTime: new Date(), // current time
+        questionType: 0, // TODO
+        modelIds: selectedModels.map((model) => model.modelid),
+        datasetId: selectedDataset.id,
       });
     } catch (error) {
       console.log(error);
@@ -36,28 +68,38 @@ export default function NewTestingPage() {
 
   return (
     <div className="flex w-full flex-col  space-y-5 rounded-2xl bg-white p-8 shadow-lg sm:p-20">
-      <Link className="btn" href="/testings" onClick={addDemoTesting}>
-        TODO: add demo testing
-      </Link>
       <h1 className="text-3xl font-bold ">创建新测试</h1>
       <div className="flex items-center space-x-3">
         <h2 className="text-xl font-bold ">选择数据集</h2>
         <p>或</p>
         {/* TODO: create new dataset */}
-        <Link className="link-primary link" href="/datasets">
+        <Link className="link-primary link" href="/upload_dataset_demo">
           上传数据集
         </Link>
       </div>
-      <DatasetOption isSelected={true} />
-      <DatasetOption isSelected={false} />
+
+      {datasets.map((dataset) => (
+        <div
+          key={dataset.id}
+          onClick={() => setSelectedDataset(dataset)}
+          className="cursor-pointer"
+        >
+          <DatasetOption
+            dataset={dataset}
+            isSelected={selectedDataset && selectedDataset.id === dataset.id}
+          />
+        </div>
+      ))}
+
       <h2 className="text-xl font-bold ">选择测试方式</h2>
       {/* 客观测试 主观测试 对抗测试 三选一 */}
-      <form className="flex justify-between">
+      <div className="flex justify-between">
         <label className="label space-x-3">
           <input
             type="radio"
             name="method"
             className="radio-primary radio"
+            onChange={() => setSelectedTestingMethod("客观测试")}
           />
           <span className="label-text font-bold">客观测试</span>
         </label>
@@ -66,6 +108,7 @@ export default function NewTestingPage() {
             type="radio"
             name="method"
             className="radio-primary radio"
+            onChange={() => setSelectedTestingMethod("主观测试")}
           />
           <span className="label-text font-bold">主观测试</span>
         </label>
@@ -74,26 +117,70 @@ export default function NewTestingPage() {
             type="radio"
             name="method"
             className="radio-primary radio"
+            onChange={() => setSelectedTestingMethod("对抗测试")}
           />
           <span className="label-text font-bold">对抗测试</span>
         </label>
-      </form>
+      </div>
       <h2 className="text-xl font-bold">选择模型</h2>
       <div className="form-control w-max px-6 ">
         <label className="label cursor-pointer space-x-3">
           <input
             type="checkbox"
-            defaultChecked="checked"
             className="checkbox-primary checkbox"
+            checked={selectedModels.length === models.length}
+            onClick={
+              selectedModels.length === models.length
+                ? () => setSelectedModels([])
+                : () => setSelectedModels(models)
+            }
           />
           <span className="label-text font-bold">全选</span>
         </label>
       </div>
-      <ModelOption isSelected={true} />
-      <ModelOption isSelected={false} />
-      <ModelOption isSelected={true} />
+
+      {models.map((model) => (
+        <div
+          key={model.modelid}
+          onClick={() => {
+            const isModelSelected = selectedModels.some(
+              (selectedModel) => selectedModel.modelid === model.modelid,
+            );
+
+            setSelectedModels((prevSelectedModels) => {
+              if (isModelSelected) {
+                // Remove the model from the selected models
+                return prevSelectedModels.filter(
+                  (selectedModel) => selectedModel.modelid !== model.modelid,
+                );
+              } else {
+                // Add the model to the selected models
+                return [...prevSelectedModels, model];
+              }
+            });
+          }}
+          className="cursor-pointer"
+        >
+          <ModelOption
+            model={model}
+            isSelected={selectedModels.some(
+              (selectedModel) => selectedModel.modelid === model.modelid,
+            )}
+          />
+        </div>
+      ))}
       <div className="flex justify-end">
-        <button className="btn btn-primary w-max">创建新测试</button>
+        <button
+          className="btn btn-primary w-max"
+          onClick={addTesting}
+          disabled={
+            !selectedDataset ||
+            !selectedTestingMethod ||
+            selectedModels.length === 0
+          }
+        >
+          <Link href="/testings">创建新测试</Link>
+        </button>
       </div>
     </div>
   );
