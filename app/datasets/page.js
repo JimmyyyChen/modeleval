69bcc7371df3f22374f2ec1feb64faa22e51f788
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-16 23:32:07 +0800

    fix dataset bug

--- app/datasets/page.js
+++ app/datasets/page.js
@@ -1,15 +1,14 @@
-// import { datasets, datasetLabels } from "./data";
-// import DatasetDisplay from "./DatasetDisplay"
+import { datasets, datasetLabels } from "./data";
+import DatasetDisplay from "./DatasetDisplay"
 
 // TODO: 组件化
 export default function Home() {
   return (
-    <p>TODO: npm build error</p>
-    // <DatasetDisplay
-    //   title="数据集"
-    //   datasets={datasets}
-    //   datasetLabels={datasetLabels}
-    //   isvisitor={true}
-    // />
+    <DatasetDisplay
+      title="数据集"
+      datasets={datasets}
+      datasetLabels={datasetLabels}
+      isvisitor={true}
+    />
   );
 }
