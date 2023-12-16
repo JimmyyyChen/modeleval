1b43b63b69fea92d0a777e02a5cedb2211afd6de
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-16 21:02:55 +0800

    创建新测试页面更新

--- app/testings/new-testing/components/DatasetOption.jsx
+++ app/testings/new-testing/components/DatasetOption.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Link from "next/link";
 
-export default function DatasetOption({ isSelected }) {
-  let demoDescription =
-    " 这里是关于数据集的描述，这里是关于数据集的描述，这里是关于数据集的描述，这里是关于数据集的描述，这里是关于数据集的描述，这里是关于数据集的描述，这里是关于数据集的描述.";
-
-  if (demoDescription.length > 50) {
-    demoDescription = demoDescription.slice(0, 50) + "...";
-  }
+export default function DatasetOption({ dataset, isSelected }) {
+  const description = dataset.description;
+  const datasetName = dataset.datasetName;
+  // TODO: there should be 3 questionType
+  const questionType = dataset.questionType ? "问答题" : "选择题";
+  const sizeInMB = dataset.sizeInMB;
+  // dataset.lastUpdate is something like "2023-12-16T07:22:34.433Z"
+  const date = new Date(dataset.lastUpdate);
+  const lastUpdate = date.toLocaleString();
 
   return (
     <div
       className={` ${
-        isSelected ? "border-2 border-teal-700" : ""
+        isSelected ? "ring-2 ring-teal-700" : ""
       } w-full flex-wrap items-center space-y-2 overflow-hidden rounded-3xl border bg-base-100 p-6`}
     >
       <h2 className="flex flex-wrap items-center space-x-3 ">
@@ -22,17 +24,17 @@ export default function DatasetOption({ isSelected }) {
           }`}
         ></div>
 
-        <p className=" font-mono text-xl font-bold">mmlu_select</p>
+        <p className=" font-mono text-xl font-bold">{datasetName}</p>
         {/* TODO: link to corresponded dataset */}
         <Link className="link-primary link text-sm" href="/datasets">
           查看更多
         </Link>
       </h2>
 
-      <p className="text-sm text-gray-500">{demoDescription}</p>
+      <p className="text-sm text-gray-500">{description}</p>
 
       <div className="ml-auto space-x-1 font-medium text-gray-800">
-        选择问题 • 101KB • 2023/12/31 23:59
+        {questionType} • {sizeInMB} MB • {lastUpdate}
       </div>
     </div>
   );
