f73a88a4418e6760b3284f5a1fee3d4477012751
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-26 13:44:00 +0800

    datasets filter

--- app/datasets/page.js
+++ app/datasets/page.js
@@ -2,16 +2,29 @@ import { prisma } from "@/lib/prisma";
 
 import DatasetDisplay from "./components/DatasetDisplay";
 
-export default async function DatasetsPage({searchParams}) {
-  const query = searchParams?.query || '';
-  const filter = searchParams?.filter || '';
+export default async function DatasetsPage({ searchParams }) {
+  const query = searchParams?.query || "";
+  const filter = searchParams?.filter || "";
+  const filterList = filter ? filter.split(",") : [];
+
+  let whereCondition = {
+    datasetName: {
+      contains: query,
+    },
+  };
+
+  if (filterList.length > 0) {
+    whereCondition.label_list = {
+      some: {
+        labelName: {
+          in: filterList,
+        },
+      },
+    };
+  }
 
   const datasets = await prisma.dataset.findMany({
-    where: {
-      datasetName: {
-        contains: query,
-      },
-    },
+    where: whereCondition,
     include: {
       label_list: true,
       ChoiceQuestions: {
@@ -24,5 +37,4 @@ export default async function DatasetsPage({searchParams}) {
   });
 
   return <DatasetDisplay title="数据集" datasets={datasets} isvisitor={true} />;
-
 }
