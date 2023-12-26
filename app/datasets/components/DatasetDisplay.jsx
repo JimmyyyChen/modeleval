6ad05b5a8ce6789c8ef018510914356c4036e659
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-26 15:31:38 +0800

    bug fixed

--- app/datasets/components/DatasetDisplay.jsx
+++ app/datasets/components/DatasetDisplay.jsx
@@ -6,13 +6,8 @@ import CheckCondition from "../../components/CheckCondition";
 export default function Home({ title, datasets, isvisitor }) {
   return (
     <div className="flex flex-col space-y-3">
-      <div>
-        <CheckCondition title="数据集" labels={datasetTypes} />
-      </div>
-
-      <div>
-        <DataTable items={datasets} type="llm" isvisitor={isvisitor} />
-      </div>
+      <CheckCondition title="数据集" labels={datasetTypes} />
+      <DataTable items={datasets} type="datasets" isvisitor={isvisitor} />
     </div>
   );
 }
